62fec5d1df7926ab40314574dd523cf23c01c0c4
chengLiu <cheng@strikingly.com>
2017-08-06 13:25:45 +0800

    feat: add subscription of up vote

--- src/Environment.js
+++ src/Environment.js
@@ -1,5 +1,6 @@
 // 1
 import { GC_AUTH_TOKEN } from './constants'
+import { SubscriptionClient } from 'subscriptions-transport-ws'
 const {
     Environment,
     Network,
@@ -10,24 +11,36 @@ const {
 // 2
 const store = new Store(new RecordSource())
 
+// 4.1
+const fetchQuery = (operation, variables) => {
+  return fetch('https://api.graph.cool/relay/v1/cj5z6tva2erc10183xahffep9', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${localStorage.getItem(GC_AUTH_TOKEN)}`
+    },
+    body: JSON.stringify({
+      query: operation.text,
+      variables,
+    }),
+  }).then(response => {
+    return response.json()
+  })
+}
+
+// 4.2
+const setupSubscription = (config, variables, cacheConfig, observer) => {
+  const query = config.text
+
+  const subscriptionClient = new SubscriptionClient('wss://subscriptions.graph.cool/v1/cj5z6tva2erc10183xahffep9', {reconnect: true})
+  subscriptionClient.subscribe({query, variables}, (error, result) => {
+    observer.onNext({data: result})
+  })
+}
+
 // 3
-const network = Network.create((operation, variables) => {
-    // 4
-    return fetch('https://api.graph.cool/relay/v1/cj5z6tva2erc10183xahffep9', {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${localStorage.getItem(GC_AUTH_TOKEN)}`
-        },
-        body: JSON.stringify({
-            query: operation.text,
-            variables,
-        }),
-    }).then(response => {
-        return response.json()
-    })
-})
+const network = Network.create(fetchQuery, setupSubscription)
 
 // 5
 const environment = new Environment({
